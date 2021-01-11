389de4598944b2430f2989a8c202159ca166b6ca
maomao <1367945824@qq.com>
2021-01-11 21:02:56 +0800

    首次提交，新增utils、base、commons、web模块

---
+++ ginkgo_blog_web/src/main/java/com/ginkgoblog/web/WebApplication.java
@@ -0,0 +1,15 @@
+package com.ginkgoblog.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author maomao
+ * @date 2021-01-11
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
